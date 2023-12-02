51188874f183f3f287bf992d50284623efb3d944
divine <divineobi07@gmail.com>
2023-12-02 17:06:23 +0100

    chore: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./index.css";
+import CourseDetail from "./pages/CourseDetail";
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/course-detail" element={<CourseDetail />} />
+      </Routes>
     </>
   );
 }
